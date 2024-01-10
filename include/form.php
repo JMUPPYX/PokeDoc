3a10af1385cf352cf9baed1b42424a40df876f5f
JMUPPYX <juliemupavo@gmail.com>
2024-01-11 00:29:48 +0100

    ajout css btn

--- include/form.php
+++ include/form.php
@@ -1,45 +1,47 @@
+ <section>
  <!-- formulaire  -->
  <div class="container pt-5">
-                <form>
+                <form class ="m-auto">
                     <div>
                         <h4 class="text-uppercase fw-bold">formulaire de contact</h4>
-                        
                     </div>
                     <div class="row mt-4">
-                        <div class="col-7">
+                        <div class="col-3">
                             <input type="text" class=" form-control rounded-0" id="nomInput" placeholder="VOTRE NOM"
                                 aria-label="VOTRE NOM"> <!-- input du nom -->
                         </div>
-                        <div class="col-5">
+                        <div class="col-3">
                             <input type="text" class="form-control rounded-0" id="prenomInput"  placeholder="VOTRE PRENOM"
                                 aria-label="VOTRE PRENOM"> <!-- input du prénom -->
                         </div>
                     </div>
     
                     <div class="row mt-4">
-                        <div class="col-5">
+                        <div class="col-3">
                             <input type="tel" class="form-control rounded-0" placeholder="VOTRE TELEPHONE"
                                 aria-label="VOTRE TELEPHONE"> <!-- input du telephone -->
                         </div>
-                        <div class="col-7">
+                        <div class="col-3">
                             <input type="email" class="form-control rounded-0" placeholder="VOTRE EMAIL"
                                 aria-label=" VOTRE EMAIL"> <!-- input du mail-->
                         </div>
                     </div>
     
                     <div class="row mt-4">
-                        <div class="col">
+                        <div class="col-6">
                             <input type="text" class="bg-grey form-control rounded-0" id="sujet" placeholder="SUJET"
                                 aria-label="SUJET"> <!-- input du sujet -->
                         </div>
                     </div>
                     <div class="mt-4 h-50">
-                        <textarea class="bg-grey form-control rounded-0 h-100" id="exampleFormControlTextarea1"
+                        <textarea class="bg-grey form-control rounded-0 h-100 w-49" id="exampleFormControlTextarea1"
                             rows="6"></textarea> <!-- input du textarea-->
                     </div>
-                    <div class = "text-center pb-5">
-                    <button type="submit" class="mt-5 btn-lg rounded-0">Envoyer</button>
+                    </div>
+                    <div class = "text-center">
+                    <button type="submit" class="mt-5 btn-lg rounded-0 btn btn-bg text-dark fs-5 fw-bold btn-outline-secondary border border-3 rounded-3">Envoyer</button>
                     </div>
                 </form>
-            </div>
+        </div>
         </section>
+
